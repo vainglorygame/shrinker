9ae212a23754035be50e9125a7966544e2d525fb
schneefux <schneefux+commit@schneefux.xyz>
2017-02-20 17:33:26 +0100

    update queries to work with vainsocial.com#develop

--- queries/match.sql
+++ queries/match.sql
@@ -7,9 +7,9 @@ attributes->>'gameMode' AS "gameMode",
 COALESCE(NULLIF(attributes->>'patchVersion', ''), '0') AS "patchVersion",
 attributes->>'shardId' AS "shardId",
 attributes->'stats'->>'endGameReason' AS "endGameReason",
-attributes->'stats'->>'queue' AS "queue",
+attributes->'stats'->>'queue' AS "queue" --,
 
-relationships->'rosters'->'data'->0->>'id' as roster_1,
-relationships->'rosters'->'data'->1->>'id' as roster_2
+--relationships->'rosters'->'data'->0->>'id' as "roster_1",
+--relationships->'rosters'->'data'->1->>'id' as "roster_2"
 
 FROM "match"
