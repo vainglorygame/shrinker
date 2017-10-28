6f6e94bc6c4487d6465408aee4d0d60c55c96032
schneefux <schneefux+commit@schneefux.xyz>
2017-10-28 15:51:02 +0200

    fix enemies healing Ardan

--- worker.js
+++ worker.js
@@ -293,7 +293,8 @@ amqp.connect(RABBITMQ_URI).then(async (rabbit) => {
                 // heal actor (since 2.9)
                 if (t.type == "HealTarget"
                     && t.payload.IsHero == 1
-                    && ["Buff_SpawnStage_Recharge", "Buff_Ace"].indexOf(t.payload.Source) != -1) // TODO LOL what?
+                    && ["Buff_SpawnStage_Recharge", "Buff_Ace"].indexOf(t.payload.Source) != -1  // TODO LOL what?
+                    && t.payload.Team != t.payload.TargetTeam)  // enemies "heal" Ardan due to his perk WTF
                     t.actor = participants.filter((p) =>
                         p.actor == t.payload.Actor
                         && p.team == t.payload.Team)[0];
